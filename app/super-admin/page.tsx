9368abfd4524ec2e9a0f5ff5049cb3026f2615ed
alexglexxx <alexglex@gmail.com>
2026-04-10 12:33:56 -0600

    Actualizar page.tsx

--- app/super-admin/page.tsx
+++ app/super-admin/page.tsx
@@ -68,8 +68,7 @@ export default function SuperAdminPage() {
       unsubscribeTenants();
       unsubscribeAppts();
       unsubscribeOrders();
-    };
-  }, []);
+    }[]);
 
   const handleCreateBiz = async (e: React.FormEvent) => {
     e.preventDefault();
